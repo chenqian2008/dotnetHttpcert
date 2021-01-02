c98930267a95a532b3fe7f14dbbcf47f2bfc8028
gavin <dz45693@sina.com>
2021-01-02 20:36:05 +0800

    initi

---
+++ HttpDemo/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HttpDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+
+                    webBuilder.ConfigureKestrel(kerstrel =>
+                    {
+                        kerstrel.ConfigureHttpsDefaults(https =>
+                        {
+                            var serverPath = AppDomain.CurrentDomain.BaseDirectory + "cert\\server.pfx";
+                            var serverCertificate = new X509Certificate2(serverPath, "123456789");
+                            https.ServerCertificate = serverCertificate;
+                            https.ClientCertificateMode = ClientCertificateMode.AllowCertificate;
+                            https.SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls | SslProtocols.None | SslProtocols.Tls11;
+                            https.ClientCertificateValidation = (cer, chain, error) =>
+                                       {
+                                           return chain.Build(cer);
+                                       };
+
+                        });
+                    });
+                });
+    }
+}
---
+++ demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Reflection;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var handler = new HttpClientHandler()
+            {
+                SslProtocols = SslProtocols.Tls12,
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                ServerCertificateCustomValidationCallback = (message, cer, chain, errors) =>
+                {
+                    return chain.Build(cer);
+                }
+            };
+            var path = AppDomain.CurrentDomain.BaseDirectory + "cert\\client.pfx";
+            var crt = new X509Certificate2(path, "123456789");
+            handler.ClientCertificates.Add(crt);
+
+            var client = new HttpClient(handler);
+            var url = "https://localhost:5001/WeatherForecast";
+            var response = client.GetAsync(url).Result;
+            Console.WriteLine(response.IsSuccessStatusCode);
+            var back = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(back);
+        }
+      
+    }
+}
